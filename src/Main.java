cb9905d876848587d6cf78b78fbf408e0af13177
Marek Padlewski <marek.padlewski@gmail.com>
2018-06-08 23:56:38 +0200

    Add main and simple menu

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,51 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
-public class Main {
-    public Main() {
+public class Main  extends StateBasedGame
+{
+    private static final String name = "Name of the game";
+    private final static int menu = 0;
+    public Main(String name) throws SlickException {
+        super(name);
+        this.addState(new Menu());
+        //this.addState(new Intro());
+        //this.addState(new Level1("/res/maps/level1.tmx"));
+        //this.addState(new Level("/res/worlds/map4.tmx",1));
+        //this.addState(new Level("/res/worlds/map2.tmx",2));
+        //this.addState(new Level("/res/worlds/map3.tmx",3));
+        //this.addState(new Level("/res/worlds/map5.tmx",4));
+
+    }
+
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.enterState(0);
+    }
+    public static void main(String[] args) throws SlickException, LWJGLException {
+
+        AppGameContainer appgc = null;
         try {
-            Display.setDisplayMode(new DisplayMode(800, 600));
-            Display.create();
-
-            while(!Display.isCloseRequested()) {
-                Display.update();
-            }
-
-            Display.destroy();
-        } catch(LWJGLException e) {
+            appgc = new AppGameContainer(new Main(name));
+        } catch (SlickException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        appgc.setTargetFrameRate(60);
+        //   appgc.setFullscreen(true);
+        appgc.setShowFPS(true);
+
+        DisplayMode[] modes = Display.getAvailableDisplayModes();
+
+        appgc.setDisplayMode(modes[0].getWidth(),modes[0].getHeight(), true);
+
+        appgc.start();
+
     }
 
-    public static void main(String[] args) {
-        new Main();
-    }
+
 }
---
+++ src/Menu.java
@@ -0,0 +1,99 @@
+import org.lwjgl.input.Mouse;
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.tiled.TiledMap;
+
+public class Menu extends BasicGameState {
+
+    public Menu(){
+        super();
+    }
+    Image background;
+    Image b_play;
+    Image b_play_border;
+    Image b_exit;
+    Image b_exit_border;
+
+    Rectangle playBox;
+    Rectangle exitBox;
+
+    int mouseX;
+    int mouseY;
+
+    boolean exit_flag = false;
+    boolean play_flag = false;
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+        background = new Image("/res/textures/menu/background.jpg");
+
+        b_play = new Image("res/textures/menu/button_play.png");
+        b_play_border = new Image("/res/textures/menu/button_play_border.png");
+        playBox = new Rectangle(gameContainer.getWidth()/6,gameContainer.getHeight()/3,b_play.getWidth(),b_play.getHeight());
+
+        b_exit = new Image("/res/textures/menu/button_exit.png");
+        b_exit_border = new Image("/res/textures/menu/button_exit_border.png");
+        exitBox = new Rectangle(gameContainer.getWidth()/6,gameContainer.getHeight()/2,b_exit.getWidth(),b_exit.getHeight());
+
+
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        g.drawImage(background,0,0);
+        g.drawImage(hoverPlay(mouseX,mouseY),playBox.getX(),playBox.getY());
+        g.drawImage(hoverExit(mouseX,mouseY),exitBox.getX(),exitBox.getY());
+
+    }
+
+    private Image hoverPlay(int x, int y){
+
+        if (playBox.contains(x,y)){
+
+            play_flag = true;
+            return b_play_border;
+        }
+        else
+        {
+            play_flag = false;
+            return b_play;
+        }
+    }
+
+    private Image hoverExit(int x, int y){
+        if (exitBox.contains(x,y)){
+
+            exit_flag = true;
+            return b_exit_border;
+        }
+        else
+        {
+            exit_flag = false;
+            return b_exit;
+        }
+    }
+
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
+        Input input = gameContainer.getInput();
+
+        mouseX = input.getMouseY();
+        mouseY = input.getMouseY();
+
+        if(exit_flag && input.isMousePressed(0)) gameContainer.exit();
+        if(play_flag && input.isMousePressed(0)) stateBasedGame.enterState(1);
+    }
+
+    @Override
+    public void enter(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+}
