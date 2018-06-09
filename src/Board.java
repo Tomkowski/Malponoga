b7946a8361e954d80c5f42439b338df1c4e901a3
Łukasz <lukstasiak26@wp.pl>
2018-06-09 12:48:28 +0200

    Fixed borders and camera

--- src/Board.java
+++ src/Board.java
@@ -8,10 +8,10 @@ public class Board extends BasicGameState{
 
     Image background;
     Animation footballer1, movingUp, movingDown, movingLeft, movingRight;
-    float fpositionX=200;
-    float fpositionY=-200;
-    float shiftX=fpositionX+200; /// na środku ma być uzależnić od danych użytkownika
-    float shiftY=fpositionY+865; /// tu też
+    float shiftX=700;
+    float shiftY=850;
+    float fpositionX=shiftX-700; /// na środku ma być uzależnić od danych użytkownika
+    float fpositionY=-200; /// tu też
     int [] duration = {200,200};
     @Override
     public int getID() {
@@ -21,55 +21,55 @@ public class Board extends BasicGameState{
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
         background = new Image("/res/textures/board/football_pitch.png");
-        Image [] walkup = {new Image ("/res/textures/footballers/up1.png"), new Image ("/res/textures/footballers/up1.png")};
-        Image [] walkdown = {new Image ("/res/textures/footballers/down1.png"), new Image ("/res/textures/footballers/down1.png")};
         Image [] walkleft = {new Image ("/res/textures/footballers/left1.png"), new Image ("/res/textures/footballers/left1.png")};
         Image [] walkright = {new Image ("/res/textures/footballers/right1.png"), new Image ("/res/textures/footballers/right1.png")};
-        movingUp = new Animation(walkup, duration, false);
         movingLeft = new Animation(walkleft, duration, false);
         movingRight = new Animation(walkright, duration, false);
-        movingDown = new Animation(walkdown, duration, false);
-        footballer1 = movingDown;
+        footballer1 = movingLeft;
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-        g.drawImage(background,fpositionX,fpositionY);
+        //background.draw(shiftX,shiftY,1.2f);
+        g.translate((int)fpositionX,(int)fpositionY);
+        g.drawImage(background,0 ,0);
         footballer1.draw(shiftX,shiftY);
         g.drawString("Pozycja piłkarza\n X: "+fpositionX+" Y:"+fpositionY,400,22);
     }
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int delta) throws SlickException {
         Input input = gameContainer.getInput();
-        /*if(input.isKeyDown(Input.KEY_UP)){
-            footballer1 = movingUp;
-            fpositionY += delta * .1f;
-            if(fpositionY>160){
-                fpositionY -= delta * .1f;
-            }
-        }
-        if(input.isKeyDown(Input.KEY_DOWN)){
-            footballer1 = movingDown;
-            fpositionY -= delta * .1f;
-            if(fpositionY<-836){
-                fpositionY += delta * .1f;
-            }
-        }*/
         if(input.isKeyDown(Input.KEY_LEFT)){
             footballer1 = movingLeft;
-            fpositionX += delta * .1f;
-            if(fpositionX>318){
-                fpositionX -= delta * .1f;
+            // shiftX gracz, fpositionX kamera
+
+            System.out.println("lewy: "+shiftX+" | "+fpositionX);
+            if(shiftX<=700&&shiftX>=0){
+                shiftX -= delta * .1f;
             }
-            //if(fpositionX>-0.6){
-            //    shiftX += delta *.1f;
-            //}
+            else if(shiftX>700&&shiftX<1200){
+                shiftX -= delta *.1f;
+                fpositionX += delta *.1f;
+            }
+            else if(shiftX>=1200&&shiftX<1724){
+                shiftX -= delta *.1f;
+            }
+
+
         }
         if(input.isKeyDown(Input.KEY_RIGHT)){
             footballer1 = movingRight;
-            fpositionX -= delta * .1f;
-            if(fpositionX<-1283){
-                fpositionX += delta * .1f;
+            System.out.println("prawy: "+shiftX+" | "+fpositionX);
+            if(shiftX>=-2&&shiftX<=700){
+                shiftX += delta * .1f;
+                //fpositionX += delta *.1f;
+            }
+            else if(shiftX>700&&shiftX<1200){
+                shiftX += delta *.1f;
+                fpositionX -= delta *.1f;
+            }
+            else if(shiftX>=1200&&shiftX<1722) {
+                shiftX += delta * .1f;
             }
         }
     }
